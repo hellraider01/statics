b3328bb0b8960f532a35f67f173d5a2c22dbc561
Sergio Casado <hellraider01@gmail.com>
2016-12-12 03:01:07 +0100

    IC(intervalo confianza) añadido

--- functions/estadisticas.R
+++ functions/estadisticas.R
@@ -1,14 +1,19 @@
 regresion <- function(frame){
   val <- scale(frame$Per,center = TRUE, scale = TRUE)
-  print(val)
   return(val)
 }
 
+# falta normalizar entre dos numeros 0-40 por ejemplo
 normalizar <- function(frame){
   des <- sd(frame$Per, na.rm = TRUE)
   media <- mean(frame$Per)
-  normal <- dnorm(frame$Per, media,des)
+  normal <- dnorm(frame$Per, media, des)
   return(normal)
+}
+
+IC <- function(normaldata, porcentaje){
+  ic <- c(t.test(normaldata, conf.level = porcentaje/100)[4]$conf.int[1],t.test(normaldata, conf.level =  porcentaje/100)[4]$conf.int[2])
+  return(ic)
 }
 
 graficar <- function(datosX, datosY){
